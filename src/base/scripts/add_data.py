bb53779321cec0d4dd5ef56a70071da660227bcb
Darlon <darlonv@gmail.com>
2023-09-01 15:51:34 +0000

    script for adding users

--- src/base/scripts/add_data.py
+++ src/base/scripts/add_data.py
@@ -4,13 +4,30 @@ from django.contrib.auth.models import User
 
 import json
 
-USERS_FILE = "users.json"
+USERS_FILE = "scripts/users.json"
+TRABALHOS_FILE = "scripts/trabalhos.json"
+AVALIACOES_FILE = "scripts/avaliacoes.json"
 
 
 def run():
     print("Running script")
 
     ##adicionando usuários
-    with open(USERS_FILE) as file:
+    print("== Adicionando usuários ==")
+    with open(USERS_FILE, "r") as file:
         users = json.load(file)
-        print(users)
+        for username in users:
+            print(f"Adicionando usuário {username}...", end=" ")
+
+            # Verifica se esse usuário já existe
+            user = User.objects.filter(username=username).first()
+            if user:
+                print("usuário já cadastrado.")
+            else:
+                email = users[username]["email"]
+                password = users[username]["password"]
+                user = User.objects.create_user(
+                    username=username, email=email, password=password
+                )
+                user.save()
+                print(f"feito.")
