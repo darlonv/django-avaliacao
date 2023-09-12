7f0ce865da74b633e8ce89b5c92385d67783e790
Darlon <darlonv@gmail.com>
2023-09-12 03:01:30 +0000

    included version 3.0 (linux) with libraries to handle: - qrcode - pdf

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="1.0"
+VERSION="1.1"
 NAME="django-avaliacao"
 DOCKERHUB_USER="darlon"
 
@@ -8,8 +8,12 @@ DOCKERHUB_USER="darlon"
 IMAGE="${DOCKERHUB_USER}/${NAME}:${VERSION}"
 IMAGE_LATEST="${DOCKERHUB_USER}/${NAME}"
 
+echo $IMAGE
+echo $IMAGE_LATEST
+
 docker build --no-cache -t ${IMAGE} .
 docker tag ${IMAGE} ${IMAGE_LATEST}
 
-# docker login
-# docker push ${IMAGE} ${IMAGE_LATEST}
+docker login
+docker push ${IMAGE}
+docker push ${IMAGE_LATEST}
